1f317529564a1760d64101e2d0ce15d05f22a0d1
elosine <justinwenloyang@gmail.com>
2020-09-15 20:35:58 -0400

    uu

--- public/notation.js
+++ public/notation.js
@@ -35,7 +35,7 @@ var pausedTime = 0;
 var animationGo = true;
 // SOCKET IO ------------------------ >
 // var socket = io();
-var socket = io('https://salty-scrubland-85563.herokuapp.com/');
+var socket = io('https://salty-scrubland-85563.herokuapp.com:5000', {path: '/socket.io'});
 // Socket.io receiver to start piece on reicept of 'startpiecebroadcast' msg
 socket.on('startpiecebroadcast', function(data) {
   if (startPieceGate) {
